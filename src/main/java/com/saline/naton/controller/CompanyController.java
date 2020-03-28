3bbfbf6e0d9f53dbe56d57489f58a8a7364d9a49
leosaline <leosaline@gmail.com>
2020-03-28 16:55:49 -0300

    add method

--- src/main/java/com/saline/naton/controller/CompanyController.java
+++ src/main/java/com/saline/naton/controller/CompanyController.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,5 +30,13 @@ public class CompanyController {
 	public ResponseEntity<Collection<Company>> listProducts() {
 		return ResponseEntity.ok((Collection<Company>)this.repo.findAll());
 	}
+	
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Return Company by ID") })
+	@RequestMapping(value = "/company/{id}", method = RequestMethod.GET, produces = "application/json")
+	@CrossOrigin(origins = {"http://localhost:8080", " http://natonfrontend:8080"})
+	@ResponseBody
+	public ResponseEntity<Company> companyById(@PathVariable Long id) {
+		return ResponseEntity.ok(this.repo.findById(id).get());
+	}	
 
 }
