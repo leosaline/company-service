8052c717c9531188d97dbca980548a9af5ebb80a
leosaline <leosaline@gmail.com>
2020-05-07 19:15:16 -0300

    refactor

--- src/main/java/com/saline/naton/entity/Company.java
+++ src/main/java/com/saline/naton/entity/Company.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="COMPANY")
 public class Company {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
