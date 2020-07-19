bd76aa7724e9234e646b36cbee5e1f7679192d8e
leosaline <leosaline@gmail.com>
2020-07-18 22:09:35 -0300

    refactor

--- src/main/java/com/saline/naton/controller/CompanyController.java
+++ src/main/java/com/saline/naton/controller/CompanyController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,7 +26,7 @@ public class CompanyController {
 	private CompanyRepository repo;
 	
 	@ApiResponses(value = { @ApiResponse(code = 200, message = "Return list of companies") })
-	@RequestMapping(value = "/companies", method = RequestMethod.GET, produces = "application/json")
+	@GetMapping(value = "/companies", produces = "application/json")
 	@CrossOrigin(origins = {"http://localhost:8080", " http://natonfrontend:8080"})
 	@ResponseBody
 	public ResponseEntity<Collection<Company>> listProducts() {
